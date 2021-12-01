24b25fb156cd8cfdfdb74eb148c9e6b549aec556
lczCoder <lczcoder@163.com>
2021-12-01 17:21:12 +0800

    feat:配置路由

---
+++ config/routes.js
@@ -0,0 +1,4 @@
+export default [
+  { exact: true, path: '/', redirect: '/user' },
+  { exact: true, path: '/user', component: '@/pages/user' },
+];
--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -1,5 +1,12 @@
+import Styles from './index.less';
 function User() {
-  return <div>这是用户界面</div>;
+  return (
+    <div className={Styles.warp}>
+      <p className={Styles.title}>这是用户界面</p>
+      <div className={Styles.box}>
+        <p className={Styles.title}>搜索🔍</p>
+      </div>
+    </div>
+  );
 }
-
 export default User;
