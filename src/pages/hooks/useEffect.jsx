f824dde61244249fe16e6cf2497c373110de85b8
lczCoder <lczcoder@163.com>
2022-01-10 18:44:45 +0800

    pref:useEffect第二参数

--- src/pages/hooks/useEffect.jsx
+++ src/pages/hooks/useEffect.jsx
@@ -1,10 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Effect = () => {
+  let [num, setNum] = useState(0);
+  // 第二参数传入一个空数组,effect只在首次加载的时候触发
+  // useEffect(() => {
+  //   console.log('生命周期钩子');
+  // }, []);
+
+  // 第二参数不传的时候，会在组件首次加载和更新的时候触发
+  // useEffect(() => {
+  //   console.log('生命周期钩子');
+  // });
+
+  // 第二参数传入一个变量的时候，会在该变量状态更新的时候触发
+  // useEffect(() => {
+  //   console.log('生命周期钩子');
+  // },[num]);
+
+  // effect中返回一个函数的时候，会在组件销毁的时候触发
   useEffect(() => {
-    console.log('生命周期钩子');
-  }, []);
-  return <div>生命周期钩子</div>;
+    console.log('num触发了');
+    return () => {
+      console.log('组件被销毁了');
+    };
+  }, [num]);
+  return (
+    <div>
+      <hr />
+      <h1>useEffect</h1>
+      <h3>数量:{num}</h3>
+      <button onClick={() => setNum(num + 1)}>改变</button>
+    </div>
+  );
 };
 
 export default Effect;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './index.less';
 import UseState from './hooks/useState.jsx';
 import Effect from './hooks/useEffect.jsx';
 export default function IndexPage() {
+  let [isOk, setIsOk] = useState(true);
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
       <UseState></UseState>
-      <Effect></Effect>
+      {isOk ? <Effect></Effect> : ''}
+      <button onClick={() => setIsOk(!isOk)}>切换Effect</button>
     </div>
   );
 }
