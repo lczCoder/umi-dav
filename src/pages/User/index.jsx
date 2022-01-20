ae28a42d518b7002850919c5969ec6889e127264
lczCoder <lczcoder@163.com>
2022-01-21 00:26:11 +0800

    feat:@connect装饰+class组件使用

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { connect } from 'dva';
 
-function User(props) {
-  console.log('props', props);
-  return <h3>User视图</h3>;
+@connect(({ user }) => ({
+  ...user,
+}))
+export default class User extends React.Component {
+  constructor(props) {
+    console.log(props);
+    super();
+  }
+  render() {
+    return <h3>User视图</h3>;
+  }
 }
 
-export default connect(({ user }) => ({
-  ...user,
-}))(User);
+// const User = (props)=> {
+//   console.log('props', props);
+//   return <h3>User视图</h3>;
+// }
+
+// export default connect(({ user }) => ({
+//   ...user,
+// }))(User);
