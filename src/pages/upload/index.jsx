9e363381220b4e0d2988e16e7d52c33a7da80972
lczCoder <lczcoder@163.com>
2022-02-03 23:34:54 +0800

    feat:上传文件

--- src/pages/upload/index.jsx
+++ src/pages/upload/index.jsx
@@ -6,7 +6,8 @@ const Upload = () => {
   const [name, setName] = useState('');
   const [address, setAddress] = useState('');
   const [age, setAge] = useState('');
-
+  const [file, setFile] = useState('');
+  // json
   const submitHandler1 = () => {
     axios
       .post('http://localhost:5050/people/boy', {
@@ -17,15 +18,24 @@ const Upload = () => {
         console.log('res', res);
       });
   };
-
+  // formdata
   const submitHandler2 = () => {
-    console.log('zzz', age);
     let formData = new FormData();
     formData.append('age', age);
     // 发送 POST 请求
     axios({
       method: 'post',
       url: 'http://localhost:5050/people/girl',
+      data: formData,
+    });
+  };
+  // 图片文件上传
+  const submitHandler3 = (e) => {
+    let formData = new FormData();
+    formData.append('imgs', file);
+    axios({
+      method: 'post',
+      url: 'http://localhost:5050/people/file',
       data: formData,
     });
   };
@@ -57,6 +67,12 @@ const Upload = () => {
             onChange={(v) => setAge(v.target.value)}
           />
           <Button type="primary" onClick={submitHandler2}>
+            提交
+          </Button>
+        </Card>
+        <Card title="图片上传">
+          <input type="file" onChange={(v) => setFile(v.target.files[0])} />
+          <Button type="primary" onClick={submitHandler3}>
             提交
           </Button>
         </Card>
