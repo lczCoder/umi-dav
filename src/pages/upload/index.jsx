26a4f4e813c773d6568b8e08f6f17f0abf1ef691
lczCoder <lczcoder@163.com>
2022-02-03 21:53:51 +0800

    feat:fromdata格式数据请求

--- src/pages/upload/index.jsx
+++ src/pages/upload/index.jsx
@@ -5,8 +5,9 @@ import { Input, Button, Space, Card } from 'antd';
 const Upload = () => {
   const [name, setName] = useState('');
   const [address, setAddress] = useState('');
+  const [age, setAge] = useState('');
 
-  const submitHandler = () => {
+  const submitHandler1 = () => {
     axios
       .post('http://localhost:5050/people/boy', {
         name,
@@ -17,11 +18,23 @@ const Upload = () => {
       });
   };
 
+  const submitHandler2 = () => {
+    console.log('zzz', age);
+    let formData = new FormData();
+    formData.append('age', age);
+    // 发送 POST 请求
+    axios({
+      method: 'post',
+      url: 'http://localhost:5050/people/girl',
+      data: formData,
+    });
+  };
+
   return (
     <div>
       <h3>后台接口上传</h3>
       <Space direction="vertical">
-        <Card title="后端JSON接口通讯">
+        <Card title="后端JSON接口通讯--JSON">
           <Input
             style={{ width: '300px' }}
             placeholder="姓名"
@@ -32,13 +45,20 @@ const Upload = () => {
             placeholder="地址"
             onChange={(v) => setAddress(v.target.value)}
           />
-          <Button type="primary" onClick={submitHandler}>
+          <Button type="primary" onClick={submitHandler1}>
             提交
           </Button>
         </Card>
-        <Card title="Card">
-          <p>Card content</p>
-          <p>Card content</p>
+        <Card title="后端JSON接口通讯--FormData">
+          <Input
+            type="text"
+            style={{ width: '300px' }}
+            placeholder="formData"
+            onChange={(v) => setAge(v.target.value)}
+          />
+          <Button type="primary" onClick={submitHandler2}>
+            提交
+          </Button>
         </Card>
       </Space>
     </div>
