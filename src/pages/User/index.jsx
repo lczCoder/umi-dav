8301b60de2bdb4231696989361477a93096f230d
lczCoder <lczcoder@163.com>
2022-02-10 23:24:31 +0800

    feat:dva状态通信

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import { connect } from 'dva';
 
-@connect(({ user }) => ({
-  ...user,
-}))
-export default class User extends React.Component {
-  constructor(props) {
-    console.log(props);
-    super();
-  }
-  render() {
-    return <h3>User视图</h3>;
-  }
-}
-
-// const User = (props)=> {
-//   console.log('props', props);
-//   return <h3>User视图</h3>;
+// @connect(({ user }) => ({
+//   ...user,
+// }))
+// export default class User extends React.Component {
+//   constructor(props) {
+//     console.log(props);
+//     super();
+//   }
+//   render() {
+//     return <h3>User视图</h3>;
+//   }
 // }
 
-// export default connect(({ user }) => ({
-//   ...user,
-// }))(User);
+const User = (props) => {
+  const { name } = props;
+  return (
+    <>
+      <h3>User视图</h3>
+      <p>{name}</p>
+    </>
+  );
+};
+
+export default connect(({ user }) => ({
+  ...user,
+}))(User);
