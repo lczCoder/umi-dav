4322d9a6656f94fc1be25fc72c2d207634c3c296
lczCoder <lczcoder@163.com>
2022-02-11 14:59:34 +0800

    feat:添加监控csi

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,18 @@
 import React, { useState } from 'react';
 import { connect } from 'dva';
 import Upload from '../upload';
+import CSI from 'csijs';
 const IndexPage = (props) => {
+  const csi = new CSI({
+    feID: 'umi', // 项目id，日志区分项目使用
+    report: (lines) => {
+      // todo 自定义你的上报逻辑
+      // console.log('error lins', lines);
+    },
+  });
+  try {
+    throw new Error('2131');
+  } catch (error) {}
   return (
     <div>
       <h1>首页</h1>
