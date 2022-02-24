b79266fb2d41a1f56acae87d3f85b5a2c799215e
lczCoder <lczcoder@163.com>
2022-02-25 00:02:14 +0800

    feat:拖拽demo示例实现

--- src/pages/Demo/index.jsx
+++ src/pages/Demo/index.jsx
@@ -1,31 +1,82 @@
-import React from 'react';
-import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import React, { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 const Index = () => {
   const onDragStart = () => {};
   const onDragUpdate = () => {};
-  const onDragEnd = () => {};
+  const grid = 8;
+  const getItemStyle = (isDragging, draggableStyle) => ({
+    userSelect: 'none',
+    padding: grid * 2,
+    margin: `0 ${grid}px 0 0`,
+    background: isDragging ? 'lightgreen' : 'grey',
+    ...draggableStyle,
+  });
+  const getListStyle = (isDraggingOver) => ({
+    background: isDraggingOver ? 'lightblue' : 'lightgrey',
+    display: 'flex',
+    padding: grid,
+    overflow: 'auto',
+  });
+
+  const [items, setItems] = useState([
+    { id: 'item-0', content: 'hello' },
+    { id: 'item-1', content: 'I' },
+    { id: 'item-2', content: 'am' },
+    { id: 'item-3', content: '卡' },
+    { id: 'item-4', content: '特' },
+    { id: 'item-5', content: '洛' },
+  ]);
+
+  const reOrder = (startIndex, endIndex) => {
+    const result = items;
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+    return result;
+  };
+
+  const onDragEnd = (result) => {
+    if (!result.destination) {
+      return;
+    }
+
+    const items = reOrder(result.source.index, result.destination.index);
+    setItems(items);
+  };
+
   return (
     <>
-      <DragDropContext
-        onDragStart={onDragStart}
-        onDragUpdate={onDragUpdate}
-        onDragEnd={onDragEnd}
-      >
-        <Droppable droppableId="droppable-1">
-          {(provided, snapshot) => (
-            <div
-              ref={provided.innerRef}
-              style={{
-                backgroundColor: snapshot.isDraggingOver ? 'blue' : 'grey',
-              }}
-              {...provided.droppableProps}
-            >
-              <h2>I am a droppable!</h2>
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+      <div className="App">
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="droppable" direction="horizontal">
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                style={getListStyle(snapshot.isDraggingOver)}
+                {...provided.droppableProps}
+              >
+                {items.map((item, index) => (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps.style,
+                        )}
+                      >
+                        {item.content}
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </div>
     </>
   );
 };
