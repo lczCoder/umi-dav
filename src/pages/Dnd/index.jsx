52bfdeffd3f5b723907c767652490fa48ee55ecb
lczCoder <lczcoder@163.com>
2022-03-07 23:58:29 +0800

    feat:框架3大组件使用

--- src/pages/Dnd/index.jsx
+++ src/pages/Dnd/index.jsx
@@ -1,7 +1,42 @@
 import React, { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 const Index = () => {
-  return <h1>拖拽组件demo</h1>;
+  const dragEnd = (e) => {
+    // 拖拽结束后
+    // do something 。。。。
+  };
+  const list = [
+    { name: '万', key: '1' },
+    { name: '读', key: '2' },
+  ];
+  return (
+    <DragDropContext onDragEnd={(e) => dragEnd(e)}>
+      <div className="warp">
+        <Droppable droppableId="box" direction="horizontal">
+          {(provided) => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              {list.map((item, idx) => (
+                <Draggable draggableId={item.name} index={idx} key={item.name}>
+                  {(provided) => (
+                    <div
+                      key={item.name}
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      <span>{item.name}</span>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
+  );
 };
 
 export default Index;
